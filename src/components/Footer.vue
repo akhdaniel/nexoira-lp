8617a279a4a2707cf39d29a063af22c39dbc526c
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2025-10-22 17:15:34 +0700

    build

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,7 @@
         <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
           <i class="bi bi-robot"></i>
         </a>
-        <span class="mb-3 mb-md-0 text-muted">&copy; 2025 Nexoira, Inc</span>
+        <span class="mb-3 mb-md-0 text-muted">&copy; 2025 Nexoira Artificial Cerdas</span>
       </div>
 
       <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
